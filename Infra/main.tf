1dfa19c6f45231a4051bc27c05bcd05e06d63228
kazem khodaei <k.khodaei@gmail.com>
2024-01-14 13:08:31 +0100

    add senstive=true for output

--- Infra/main.tf
+++ Infra/main.tf
@@ -21,4 +21,5 @@ resource "azurerm_storage_account" "storage_account" {
 
 output "storage_account_connection_string" {
   value = azurerm_storage_account.storage_account.primary_connection_string
+  sensitive = true
 }
