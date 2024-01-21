b96e23c324d4501e6942cb98a8a14a0b5a2a1a00
kazem khodaei <k.khodaei@gmail.com>
2024-01-21 20:32:18 +0100

    chanser service paln to B1

--- Infra/main.tf
+++ Infra/main.tf
@@ -43,7 +43,7 @@ resource "azurerm_service_plan" "weather" {
   name                = "weather-appserviceplan"
   location            = azurerm_resource_group.terraformResource.location
   resource_group_name = azurerm_resource_group.terraformResource.name
-  sku_name            = "D1"
+  sku_name            = "B1"
   os_type             = "Windows"
 }
 
