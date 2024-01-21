3ea65f67e833547717f33abb2dfc3d7d3dce7963
kazem khodaei <k.khodaei@gmail.com>
2024-01-21 17:29:57 +0100

    update access key

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name = "tf-state-rg"
     storage_account_name = "kzterraformstate"
     container_name = "tfstate"
-    access_key = "aKe8UEChcKf0RDHQAGCRqjpR+kFhAGAjrsbCYRZWPOms6YDhxK3ZWAbOrcE98yqfjnZBXr9QBvhL+AStp/5UNA=="
+    access_key = "/0he+A4Z+5dGZ1/4YxasFq6mLBk8OfBCazi9drR65l0B5aOcgO+Br3CpTJXAXpLkL40D+IyL6NiI+AStqFBxpw=="
     key = "terraform.tfstate"
   }
 }
