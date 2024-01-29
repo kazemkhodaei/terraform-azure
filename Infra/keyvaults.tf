a8a27d1009fa32d152e9a9a64f84e287397f7abd
kazem khodaei <k.khodaei@gmail.com>
2024-01-29 22:18:08 +0100

    remove access

--- Infra/keyvaults.tf
+++ Infra/keyvaults.tf
@@ -1,25 +1,9 @@
 resource "azurerm_key_vault" "weathe-keyvault" {
   name                = "weather-kv"
-  location            = terraformResource.location
+  location            = azurerm_resource_group.terraformResource.location
   resource_group_name = var.resource_group_name
   tenant_id           = azurerm_app_service.weater_app_service.id
   sku_name            = "standard"
-  access_policy = {
-    tenant_id = azurerm_app_service.weater_app_service.id
-    object_id = azurerm_app_service.weater_app_service.object_id
-
-    key_permissions = [
-      "Get",
-    ]
-
-    secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-  }
 
 }
 
