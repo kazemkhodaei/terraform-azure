1fc1a8a37d1e564db9533d86911e14d2f0fbc4c6
kazem khodaei <k.khodaei@gmail.com>
2024-02-04 16:53:58 +0100

    remove keyvault

--- Infra/keyvaults.tf
+++ Infra/keyvaults.tf
@@ -1,10 +1,1 @@
-resource "azurerm_key_vault" "weathe-keyvault" {
-  name                = "weather-kv"
-  location            = azurerm_resource_group.terraformResource.location
-  resource_group_name = var.resource_group_name
-  tenant_id           = azurerm_app_service.weater_app_service.id
-  sku_name            = "standard"
-
-}
-
 
