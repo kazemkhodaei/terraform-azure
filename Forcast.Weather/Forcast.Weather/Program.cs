a3c3b1517a3ef53f9d8a6fac099a2654a49b9ae9
kazem khodaei <k.khodaei@gmail.com>
2024-02-18 13:02:28 +0100

    change connection string

--- Forcast.Weather/Forcast.Weather/Program.cs
+++ Forcast.Weather/Forcast.Weather/Program.cs
@@ -12,8 +12,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-var connectionString = GetConnectionString(builder);
+var connectionString = builder.Configuration.GetConnectionString("SqlDatabase")!;
 
 
 builder.Services.AddDbContext<WeatherDbContext>(option =>
