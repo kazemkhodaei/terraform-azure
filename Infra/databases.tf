d5e49e4567134103ffc59e4dce45af4619b32dc4
kazem khodaei <k.khodaei@gmail.com>
2024-03-11 21:29:56 +0100

    dbowner

--- Infra/databases.tf
+++ Infra/databases.tf
@@ -24,14 +24,14 @@ resource "azurerm_mssql_database" "weather_database" {
 resource "mssql_user" "example" {
   server {
     host = azurerm_mssql_server.sqlServer.fully_qualified_domain_name
-    azuread_managed_identity_auth {
-      
+    azuread_managed_identity_auth  {
+     
     }
   }
 
   database  = azurerm_mssql_database.weather_database.name
   username  = azurerm_user_assigned_identity.weather_user_assigned_identity.name
-
+  object_id = azurerm_user_assigned_identity.weather_user_assigned_identity.client_id
 
   roles     = ["db_owner"]
   depends_on = [azurerm_mssql_database.weather_database]
