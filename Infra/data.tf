a3100b085389419fe26dfc9d6279cd6b503be965
kazem khodaei <k.khodaei@gmail.com>
2024-08-03 11:30:20 +0200

    move to azure keyvault

--- Infra/data.tf
+++ Infra/data.tf
@@ -2,10 +2,10 @@ data "azurerm_client_config" "current" {}
 
 data "azurerm_key_vault_secret" "sql_admin_login" {
   name         = "sql-admin-login"
-  key_vault_id = data.azurerm_key_vault.mainKeyVault.id
+  key_vault_id = azurerm_key_vault.mainKeyVault.id
 }
 
 data "azurerm_key_vault_secret" "sql_admin_password" {
   name         = "sql-admin-password"
-  key_vault_id = data.azurerm_key_vault.mainKeyVault.id
+  key_vault_id = azurerm_key_vault.mainKeyVault.id
 }
